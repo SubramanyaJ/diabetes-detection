25a4abbfa527850bdae298fd28b74097dc2a4ec0
Subramanya J <subramanyaj7@gmail.com>
2025-01-06 17:31:37 +0530

    Documentation

--- src/testing/randomForest.py
+++ src/testing/randomForest.py
@@ -1,3 +1,15 @@
+# /**
+#   *   Uses the Random Forest classifier for diabetes prediction.
+#   *   Accuracy : 96.672%
+#   *   Optimal Parameters :
+#   *   Maximum Leaf Nodes : 25
+#   *   Tree Estimators : 96
+#   *   Maximum Tree Depth : 6
+#   *   
+#   *   
+#   **/
+
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
